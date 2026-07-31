4d65f775d5ece826b6d17889b5b9f2391b1f63e7
Abdullah Al Fahim <fahim.devx@gmail.com>
2026-07-31 16:15:55 +0600

    ok

--- api/index.ts
+++ api/index.ts
@@ -336,7 +336,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:3000",
       "https://mupi-lost-and-found.vercel.app",
-      "https://mupi-lost-found-client.vercel.app", // Add your frontend URL
+      "https://mupi-lost-and-found.vercel.app",
     ],
     credentials: true,
   }),
